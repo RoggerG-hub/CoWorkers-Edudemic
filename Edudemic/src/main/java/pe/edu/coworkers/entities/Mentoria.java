1390c1ac8af0ac0f4ccb0b14e63e8aa1c7cd4c6a
Rogger Barrientos Mauricio <u201924159@upc.edu.pe>
2022-05-01 01:51:32 -0500

    editar_v2

--- Edudemic/src/main/java/pe/edu/coworkers/entities/Mentoria.java
+++ Edudemic/src/main/java/pe/edu/coworkers/entities/Mentoria.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name="mentorias")
@@ -19,9 +18,6 @@ public class Mentoria {
 	private int hInicio;
 	private int hFin;
 	private Date fecha;
-	@OneToOne
-	@JoinColumn(name="estudiante_id")
-	private Estudiante estudiante;
 	@ManyToOne
 	@JoinColumn(name="profesor_id",nullable=false)
 	private Profesor profesor;
@@ -56,12 +52,7 @@ public class Mentoria {
 		this.fecha = fecha;
 	}
 
-	public Estudiante getEstudiante() {
-		return estudiante;
-	}
-	public void setEstudiante(Estudiante estudiante) {
-		this.estudiante = estudiante;
-	}
+
 	public Profesor getProfesor() {
 		return profesor;
 	}
--- Edudemic/src/main/java/pe/edu/coworkers/repository/MentoriaRepository.java
+++ Edudemic/src/main/java/pe/edu/coworkers/repository/MentoriaRepository.java
@@ -1,5 +1,32 @@
 package pe.edu.coworkers.repository;
 
-public class MentoriaRepository {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import pe.edu.coworkers.entities.Mentoria;
+
+@Named
+public class MentoriaRepository implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@PersistenceContext(unitName = "demoWeb")
+	private EntityManager em;
+
+	public Long registrarMentoria(Mentoria mentoria) throws Exception {
+		em.persist(mentoria);
+		return mentoria.getId();
+	}
+	public List<Mentoria> listarMentorias () throws Exception 
+	{
+		List<Mentoria> mentorias = new ArrayList<>();
+		TypedQuery<Mentoria> query = em.createQuery("SELECT m FROM Mentoria m",Mentoria.class);
+		mentorias = query.getResultList();
+		return mentorias;
+	}
 
 }
